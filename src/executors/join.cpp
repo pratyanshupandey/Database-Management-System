e8eef6bc56a5db385fc94876b18a366fe85c9a38
pratyanshupandey <pratyan2001@gmail.com>
2021-11-28 13:48:43 +0530

    NESTED join

--- src/executors/join.cpp
+++ src/executors/join.cpp
@@ -82,10 +82,113 @@ bool semanticParseJOIN()
     return true;
 }
 
+
 void executeNestedJOIN()
 {
     logger.log("executeNestedJOIN");
-    cout << "executeNestedJOIN" << endl;
+
+    Table table1 = *tableCatalogue.getTable(parsedQuery.joinFirstRelationName);
+    Table table2 = *tableCatalogue.getTable(parsedQuery.joinSecondRelationName);
+    Cursor cursor1 = table1.getCursor();
+
+    vector <string> resultColumns(table1.columns);
+    for (uint i = 0; i < table2.columns.size(); i++)
+        resultColumns.push_back(table2.columns[i]);
+    Table *resultTable = new Table(parsedQuery.joinResultRelationName);
+    resultTable->columns = resultColumns;
+    resultTable->columnCount = resultColumns.size();
+    resultTable->maxRowsPerBlock = (uint)((BLOCK_SIZE * 1000) / (sizeof(int) * resultTable->columnCount));
+
+
+    int firstColumnIndex = table1.getColumnIndex(parsedQuery.joinFirstColumnName);
+    int secondColumnIndex = table2.getColumnIndex(parsedQuery.joinSecondColumnName);
+
+    int table1_blocks_per_iteration = parsedQuery.joinBuffer - 2;
+    int no_of_iterations = ceil((float)table1.blockCount / (float)table1_blocks_per_iteration);
+
+    vector <vector <int>> table1_rows;
+    vector <vector <int>> table2_rows;
+    vector <vector <int>> result_rows;
+    vector <int> row;
+
+    if (table2.blockCount == 1)
+    {
+        Cursor cursor2 = table2.getCursor();
+        for (uint row_num = 0; row_num < table2.rowsPerBlockCount[0]; row_num++)
+                table2_rows.push_back(cursor2.getNext());
+    }
+
+    for (uint iter = 0; iter < no_of_iterations; iter++)
+    {
+        table1_rows.clear();
+        for (uint page_num = iter * table1_blocks_per_iteration; page_num < (iter + 1) * table1_blocks_per_iteration && page_num < table1.blockCount; page_num++)
+            for (uint row_num = 0; row_num < table1.rowsPerBlockCount[page_num]; row_num++)
+                table1_rows.push_back(cursor1.getNext());
+        
+        if (table2.blockCount == 1)
+        {   
+            for (uint row_num = 0; row_num < table2_rows.size(); row_num++)
+            {
+                row = table2_rows[row_num];
+                for (uint i = 0; i < table1_rows.size(); i++)
+                {
+                    if (evaluateBinOp(table1_rows[i][firstColumnIndex], row[secondColumnIndex], parsedQuery.joinBinaryOperator))
+                    {
+                        vector <int> new_row(table1_rows[i]);
+                        new_row.insert(new_row.end(), row.begin(), row.end());
+                        result_rows.push_back(new_row);
+                        resultTable->rowCount++;
+                    }
+                    if (result_rows.size() == resultTable->maxRowsPerBlock)
+                    {
+                        bufferManager.writePage(resultTable->tableName, resultTable->blockCount, result_rows, result_rows.size());
+                        resultTable->rowsPerBlockCount.push_back(result_rows.size());
+                        resultTable->blockCount++;
+                        result_rows.clear();
+                    }
+                }
+            }
+        }
+        else
+        {
+            Cursor cursor2 = table2.getCursor();
+            while (true)
+            {
+                row = cursor2.getNext();
+                if (row.empty())
+                    break;
+                
+                for (uint i = 0; i < table1_rows.size(); i++)
+                {
+                    if (evaluateBinOp(table1_rows[i][firstColumnIndex], row[secondColumnIndex], parsedQuery.joinBinaryOperator))
+                    {
+                        vector <int> new_row(table1_rows[i]);
+                        new_row.insert(new_row.end(), row.begin(), row.end());
+                        result_rows.push_back(new_row);
+                        resultTable->rowCount++;
+                    }
+                    if (result_rows.size() == resultTable->maxRowsPerBlock)
+                    {
+                        bufferManager.writePage(resultTable->tableName, resultTable->blockCount, result_rows, result_rows.size());
+                        resultTable->rowsPerBlockCount.push_back(result_rows.size());
+                        resultTable->blockCount++;
+                        result_rows.clear();
+                    }
+                }
+            }
+        }
+    }
+    if (!result_rows.empty())
+    {
+        bufferManager.writePage(resultTable->tableName, resultTable->blockCount, result_rows, result_rows.size());
+        resultTable->rowsPerBlockCount.push_back(result_rows.size());
+        resultTable->blockCount++;
+        result_rows.clear();
+    }
+
+    tableCatalogue.insertTable(resultTable);
+
+    cout << "\nNo of Block Accesses = " << endl;
     return;
 }
 
